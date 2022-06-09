2518aadc6dcba3c1582690dff693a206dc5b0ddb
Pedr0Henriqu <pedro.mendonca@sptech.school>
2022-06-09 19:38:14 -0300

    adicionando vetor e for na modalidade

--- public/Modalidades/modalidades.js
+++ public/Modalidades/modalidades.js
@@ -1,7 +1,34 @@
+var lista = []
+
 function home(){
     window.location.href = "../index.html"
 }
 
 function login(){
     window.location.href = "../Login/login.html"
+}
+
+function verificar(){
+    var frase = ipt_frase.value;
+    var contagem = 0;
+    lista.push(frase)
+
+    for(var i = 0; i < lista.length ; i++){
+        if(lista.length > 3){
+            contagem++
+        }
+    }
+
+    if(contagem > 0){
+        alert("Apenas 3 modalidades!")
+    }else{
+        texto.innerHTML = `
+        Sua modalidade preferida é: ${lista[0]}
+        <br>
+        Sua 2º modalidade favorita é: ${lista[1]} 
+        <br>
+        Sua 3º modalidade favorita é: ${lista[2]}
+        `
+    }
+
 }
