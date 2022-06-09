d8db2a68ced43c6d190877dcf67da9309ee7f164
Pedr0Henriqu <pedro.mendonca@sptech.school>
2022-06-09 00:32:06 -0300

    arrumando textos na página de modalidades/ index // colocando inserts no script do bd // arrumando css final nas páginas de graficos e perfil

--- Banco de Dados/script site bd.sql
+++ Banco de Dados/script site bd.sql
@@ -19,6 +19,22 @@ fkUsuario int, foreign key (fkUsuario) references usuario (idUsuario)
 select * from usuario;
 select * from votacao;
 
+insert into votacao(modalidade , skatista, fkUsuario) values
+('Bowl','Tony Hawk', 1);
+
+insert into votacao(modalidade , skatista, fkUsuario) values
+('Street','Bob Burnquist', 1);
+
+insert into votacao(modalidade , skatista, fkUsuario) values
+('Vertical','Rodney Mullen', 1);
+
+insert into votacao(modalidade , skatista, fkUsuario) values
+('FreeStyle','Nyjah Huston', 1);
+
+insert into votacao(modalidade , skatista, fkUsuario) values
+('Downhill','Rodney Mullen', 1);
+
+
 select count(idVotacao) AS 'QUANTIDADE', skatista from votacao where skatista = 'Luan de Oliveira';
 select count(idVotacao) AS 'QUANTIDADE', skatista from votacao where skatista = 'Nyjah Huston';
 select count(idVotacao) AS 'QUANTIDADE', skatista from votacao where skatista = 'Bob Burnquist';
@@ -33,6 +49,10 @@ select count(idVotacao), modalidade from votacao where modalidade = 'Downhill';
 -- SELECT * FROM usuario WHERE email = 'alex@sptech.school' AND senha = aes_encrypt('ñ ØjäàñusóÌÜÌ' , 'luanOliveira');
  insert into usuario values
 (null, 'pedro','pedro@sptech.school', aes_encrypt('12345', 'luanOliveira'));
+
+
+
+update usuario set nome = 'pedro henrique' where idUsuario = 1;
 
 -- insert into usuario values
 -- (null , 'Paulo', 'paulo.souza@sptech.school', md5('luanOliveira'));
